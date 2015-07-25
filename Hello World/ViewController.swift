8c5ea2ba3d3f28b02477311efc1e028f76f1d288
Jose Quaresma <josequaresma@gmail.com>
2015-07-25 10:13:31 +0200

    cat human years app working

--- Hello World/ViewController.swift
+++ Hello World/ViewController.swift
@@ -10,6 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var textField: UITextField!
+    
+    @IBOutlet var humanYearsLabel: UILabel!
+    
+    @IBOutlet var label: UILabel!
+    
+    @IBAction func submit(sender: AnyObject) {
+        
+        print("Button tapped")
+        
+        let catYear:Int? = Int(textField.text!)
+        
+        if ((catYear) != nil) {
+            humanYearsLabel.text = "Your cat is " + String(catYear!*7) + " human years old."
+        } else {
+            humanYearsLabel.text = "'" + textField.text! + "' is not an integer. Please try again"
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
